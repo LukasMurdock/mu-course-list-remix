8788d97be44df8d8d1f76fa78a54331d20367f17
LukasMurdock <lukas.murdock@gmail.com>
2022-11-10 14:14:34 -0500

    debug

--- app/components/search/SearchSettings.tsx
+++ app/components/search/SearchSettings.tsx
@@ -17,6 +17,7 @@ function readableTermId(string: string) {
 	)
 
 	console.log({ seasonCodeIndex })
+	console.log(Object.values(TermMap)[seasonCodeIndex])
 
 	const readableSeason = Object.entries(TermMap).find(
 		(term) => term[1] === seasonCode
